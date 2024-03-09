bef3f8002a43e833b9346489ac33314b1fb87fce
Naveen <naveensimhanaidu777@gmail.com>
2024-03-09 20:16:07 +0530

    first commit

---
+++ src/main/java/ram/in/App.java
@@ -0,0 +1,13 @@
+package ram.in;
+
+/**
+ * Hello world ram!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World ram!" );
+    }
+}
